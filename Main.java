688c1de55826a9b27842cbb5d9fff22050f5b15b
Mia <mnestrada@scl.edu.gt>
2025-05-09 16:05:43 -0600

    Version 1

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main{
+ public static void main(String[] args) {
+     //Crear una ventana con JFrame 
+     JFrame miVentana = new JFrame();
+     miVentana.setTitle("Hospital SCL");
+     miVentana.setSize(1000,800);
+     
+     //Crear un texto con JLabel
+     JLabel texto = new JLabel();
+     texto.setText("Hola mundo");
+     miVentana.add(texto);
+     
+     miVentana.setVisible(true);
+    }
+}
+
+
+     
+
